65ac49e491fb92c7d38f115a8d01ee8c3933298a
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 22:03:19 +0500

    1. Создайте свой тип исключения.
    2. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
    3. В блоке catch выведите в консольном сообщении текст исключения.

---
+++ Exceptions/Exceptions/MyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// 1. Создайте свой тип исключения.
+/// 2. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+/// Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+/// 3. В блоке catch выведите в консольном сообщении текст исключения.
+/// </summary>
+namespace Exceptions
+{
+    class MyException : Exception
+    {
+        public MyException() : base("Мое исключение")
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// 1. Создайте свой тип исключения.
+/// 2. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+/// Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+/// 3. В блоке catch выведите в консольном сообщении текст исключения.
+/// </summary>
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[5];
+            exceptions[0] = new IndexOutOfRangeException();
+            exceptions[1] = new ArgumentException();
+            exceptions[2] = new FormatException();
+            exceptions[3] = new ArithmeticException();
+            exceptions[4] = new MyException();
+            foreach(var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+    }
+}
